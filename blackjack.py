75b4f9113574fae4c8f0fd106c091e07304693d4
Daniel Weinman <namniewd@gmail.com>
2019-07-05 12:11:57 -0400

    more work on split functionality

--- blackjack.py
+++ blackjack.py
@@ -38,8 +38,29 @@ def get_hand_value(hand):
   
   return total
 
+# def handle_split(player_hand, dealer_hand, deck):
+#   # setup potential hands
+#   ph1 = []
+#   ph2 = []
+#   ph1.append(player_hand[0])
+#   ph1.append(deck.pop())
+#   ph2.append(player_hand[1])
+#   ph2.append(deck.pop())
+#   print(ph1)
+#   print(ph2)
+
+#   while get_hand_value(ph1) < 21:
+#     cmd = input(f'\nYou have {get_hand_value(ph1)} vs {card_value(dealer_hand[0])}.  What would you like to do? [h] Hit [s] Stand ')
+#     # hitting
+#     if cmd == 'h':
+#       ph1.append(deck.pop())
+#       print(get_hand_value(ph1))
+#       print('\nDealer: ' + Fore.RED + f'{dealer_hand[0]}..... {card_value(dealer_hand[0])} ' + Style.RESET_ALL)
+#       print('\nPlayer: ' + Fore.GREEN + f'{ph1}..... {get_hand_value(ph1)} ' + Style.RESET_ALL)
+
 
 # single hand loop
+# def play_hand(bank, player_hand=None, dealer_hand=None, deck=None, bet=None):
 while True:
   if bank <= 0:
     while bank <= 0:
@@ -65,19 +86,21 @@ while True:
       bet = None
 
 
-  # set up deck
+  # set up deck if it wasn't passed in to function
   ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
   suits = ['\u2664',	'\u2661',	'\u2662', '\u2667']
   deck = list(itertools.product(ranks, suits))
 
-  # shuffle the deck
+    # shuffle the deck
   random.shuffle(deck)
 
-  # initialize the hands
+  # initialize the hands if they weren't passed in to function
   player_hand = [deck.pop(), deck.pop()]
+  player_total = get_hand_value(player_hand)
+  
   dealer_hand = [deck.pop(), deck.pop()]
   dealer_total = get_hand_value(dealer_hand)
-  player_total = get_hand_value(player_hand)
+
 
   # boolean to keep track if hand ends on initital deal
   hand_over = False
@@ -112,7 +135,7 @@ while True:
     cmd = input('\nWould you like to play again?  [y] Yes  [n] No ')
     
     if cmd == 'n':
-      break
+      exit()
     elif cmd != 'y':
       print('Invalid input')
 
@@ -149,7 +172,7 @@ while True:
         # player doubles
         elif cmd == 'd':
           if int(bet) * 2 > bank:
-            cmd = input(Fore.RED + f'\nYou do not have enough funds to double down. Press any key to continue. ' + Style.RESET_ALL)
+            print(Fore.RED + f'\nYou do not have enough funds to double down. Press any key to continue. ' + Style.RESET_ALL)
             continue
           else:
             os.system('clear')
@@ -173,12 +196,19 @@ while True:
         # if user stays, break out of player loop
         elif cmd == 's':
           break
-
+        
+        # if user splits, handle split
+        # elif cmd == 'p':
+        #   if int(bet) * 2 > bank:
+        #     print(Fore.RED + "\nNot enough $ to split" + Style.RESET_ALL)
+        #     continue
+          # else:
+          #   handle_split(player_hand, dealer_hand, deck)
 
         else:
           print(Fore.RED + '\nInvalid Input' + Style.RESET_ALL)
       
-      # after initial hit when player has 3 cards, need to remove options to double
+      # after initial hit when player has 3 cards, need to remove options to double/split
       else:
         cmd = input(f'\nYou have {player_total} vs {card_value(dealer_hand[0])}.  What would you like to do? [h] Hit [s] Stand ')
         # if user hits
@@ -271,8 +301,8 @@ while True:
               else:
                 bank += int(bet)
                 break
-    
-    
+
+
 
 
 
